e06ad4cbd98a0e11ba51c39125eb20299ba4ec85
dante <dante@dante-PC>
2012-05-20 12:59:28 +0200

    commit 2.1 print

--- matareyes/src/matareyes.java
+++ matareyes/src/matareyes.java
@@ -5,8 +5,8 @@ public class matareyes {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// version 2 print
-		System.out.println("version 2 print");
+		// version 2.1 print
+		System.out.println("version 2.1 print");
 
 	}
 
