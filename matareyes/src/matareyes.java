a317a2b0ec2fc5fb6daeab53a241fa3fd6062300
dante <dante@dante-PC>
2012-05-20 13:09:04 +0200

    commit version 4.1 print

--- matareyes/src/matareyes.java
+++ matareyes/src/matareyes.java
@@ -5,8 +5,8 @@ public class matareyes {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// version 4 print
-		System.out.println("version 4 print");
+		// version 4.1 print
+		System.out.println("version 4.1 print");
 	}
 
 }
